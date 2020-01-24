767443176808a50d2dd3158f6fee9cbe6f6836a2
ibuziuk <ilyabuziuk@gmail.com>
2020-01-24 09:41:59 +0000

    Display title in the UpperCase
    Signed-off-by: ibuziuk <ilyabuziuk@gmail.com>

--- quarkus-backend/src/main/java/org/eclipse/chedemos/Post.java
+++ quarkus-backend/src/main/java/org/eclipse/chedemos/Post.java
@@ -24,7 +24,7 @@ public class Post extends PanacheMongoEntity {
     }
 
     public String getTitle() {
-        return title;
+        return title.toUpperCase();
     }
 
     public String getContent() {
