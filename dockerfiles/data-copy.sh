491b3b0db6db430e358f1009d5af7c4dbada33a2
user1 <sutan@redhat.com>
2020-04-15 13:52:42 +0000

    Adding dockerfile for data container
    Signed-off-by: Sun Tan <sutan@redhat.com>

---
+++ dockerfiles/data-copy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ ! -d /work/volumes/.m2/repository ]; then
+  mkdir -p /work/volumes/.m2/repository;
+  cp -rf /work/m2repo/* /work/volumes/.m2/repository/;
+fi
+
+if [ ! -d /projects/postit-app/node-frontend/node_modules ]; then
+  while [ ! -d /projects/postit-app/node-frontend ]; do
+    echo '/projects/postit-app/node-frontend folder not created yet ...retry in 1sec';
+    sleep 1;
+  done
+  echo 'copying /work/node_modules/* to /projects/postit-app/node-frontend/node_modules';
+  mkdir -p /projects/postit-app/node-frontend/node_modules;
+  cp -rf /work/node_modules/* /projects/postit-app/node-frontend/node_modules;
+fi
+sleep infinity
+
