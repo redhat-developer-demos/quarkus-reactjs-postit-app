5a48f7da7e6bccaa016e3d7c7fa79db39b5d090d
sunix <sutan@redhat.com>
2020-11-24 13:59:31 +0000

    using the uppercase service + button to change title in uppercase

--- node-frontend/src/setupProxy.js
+++ node-frontend/src/setupProxy.js
@@ -22,5 +22,24 @@ module.exports = function (app) {
         })
     );
 
+
+    var uppercase_quarkus_host = 'localhost';
+    var uppercase_quarkus_port = '8082';
+
+    if (process.env.COMPONENT_QUARKUS_UPPERCASE_HOST) {
+        uppercase_quarkus_host = process.env.COMPONENT_QUARKUS_UPPERCASE_HOST;
+    }
+
+    if (process.env.COMPONENT_QUARKUS_UPPERCASE_PORT) {
+        uppercase_quarkus_port = process.env.COMPONENT_QUARKUS_UPPERCASE_PORT;
+    }
+
+    app.use(
+        '/touppercase',
+        createProxyMiddleware({
+            target: `http://${uppercase_quarkus_host}:${uppercase_quarkus_port}`,
+            changeOrigin: true
+        })
+    );
 };
 
