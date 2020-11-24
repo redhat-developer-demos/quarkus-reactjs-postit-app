51f433c17132c7f55489b3ab048089b4c9fff6aa
sunix <sutan@redhat.com>
2020-11-24 14:19:14 +0000

    updating copy script after renaming the project location
    Signed-off-by: sunix <sutan@redhat.com>

--- dockerfiles/data-copy.sh
+++ dockerfiles/data-copy.sh
@@ -4,14 +4,14 @@ if [ ! -d /work/volumes/.m2/repository ]; then
   cp -rf /work/m2repo/* /work/volumes/.m2/repository/;
 fi
 
-if [ ! -d /projects/postit-app/node-frontend/node_modules ]; then
-  while [ ! -d /projects/postit-app/node-frontend ]; do
-    echo '/projects/postit-app/node-frontend folder not created yet ...retry in 1sec';
+if [ ! -d /projects/quarkus-reactjs-postit-app/node-frontend/node_modules ]; then
+  while [ ! -d /projects/quarkus-reactjs-postit-app/node-frontend ]; do
+    echo '/projects/quarkus-reactjs-postit-app/node-frontend folder not created yet ...retry in 1sec';
     sleep 1;
   done
   sleep 5;
-  echo 'copying /work/node_modules/* to /projects/postit-app/node-frontend/node_modules';
-  cd /projects/postit-app/node-frontend/;
+  echo 'copying /work/node_modules/* to /projects/quarkus-reactjs-postit-app/node-frontend/node_modules';
+  cd /projects/quarkus-reactjs-postit-app/node-frontend/;
   tar -zxvf /work/node_modules.tar.gz
 fi
 sleep infinity
